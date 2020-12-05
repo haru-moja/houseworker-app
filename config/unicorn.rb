2f16095ede3f06f0eafbccd1ffcd88dd9950e6d3
haruharu <haruka373000@gmail.com>
2020-12-06 00:59:30 +0900

    修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,11 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
+app_path = File.expand_path('../../../', __FILE__)  # 「../」が一つ増えている
 
 #アプリケーションサーバの性能を決定する
 worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory "#{app_path}/current/config/"  # 「current」を指定
+working_directory "#{app_path}/current"  # 「current」を指定
 
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"  # 「shared」の中を参照するよう変更
@@ -17,7 +17,7 @@ listen "#{app_path}/shared/tmp/sockets/unicorn.sock"  # 「shared」の中を参
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"  # 「shared」の中を参照するよう変更
 
 #通常のログを記録するファイルを指定
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log" 
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"  # 「shared」の中を参照するよう変更
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
